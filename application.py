bc8c532dd3d35e4b9f3dd6b974b80c2c4c2c0200
Quinton Harris <quinton.harris@gmail.com>
2020-03-20 22:41:13 -0700

    print random quote per prompt

--- application.py
+++ application.py
@@ -1,21 +1,31 @@
+from random import sample
 import praw
+import yaml
 import os 
+
+with open(r'quotes.yaml') as file:
+    quotes = yaml.load(file, Loader=yaml.FullLoader)
 
 reddit = praw.Reddit(client_id=os.environ['REDDIT_API_KEY'],
                      client_secret=os.environ['REDDIT_API_SECRET'],
                      user_agent='bot-in-black')
 
 submissions = reddit.subreddit('westworld').hot(limit=10) 
+comments = [c for s in submissions for c in s.comments]
 
-for submission in submissions:
-    # print(f"For submission {submission.id}")
+def summarize_comment(comment):
+    author_name = "(Deleted)" if comment.author is None else comment.author.name
+    print(f'{author_name}: {comment.body[0:64]}')
 
-    for comment in submission.comments:
-        if comment.author is None:
-            comment_author_name = "(Deleted)"
-        else:
-            comment_author_name = comment.author.name
+def add_comment(comment):
+    print(f"Do sometihng with {comment.id}")
+    print(sample(quotes, 1)[0])
 
-        # print(f'{comment_author_name}: {comment.body}')
-        if "Man in Black" in comment.body:
-            print(f"Do something with {comment.id}")
+for comment in comments:
+    # summarize_comment(comment)
+
+    if "Rehoboam" in comment.body:
+        add_comment(comment)
+
+# RAW t1_fl2qtzp
+# DZNQBIT POST ID fl2qtzp
